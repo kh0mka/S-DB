189b23b9b4c8290baa5a469b2c16cf6a68285ccb
kh0mka <kirya.khomenok@mail.ru>
2022-11-26 02:32:44 +0300

    Dapper, MySqlClient + Controller&Class Tariff

---
+++ Beltelecom/Controllers/TariffController.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace Beltelecom.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TariffController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        
+        public TariffController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Tariff>>> GetAllTariff()
+        {
+            var connectionString = _config.GetConnectionString("DbConnection");
+            await using var connection = new MySqlConnection(connectionString);
+            // using var connection = new SqlConnection(_config.GetConnectionString("DbConnection"));
+            var alltariff = await connection.QueryAsync<Tariff>("SELECT * FROM Tariff");
+            return Ok(alltariff);
+        }
+
+        [HttpGet("id={tariffId}")]
+
+        public async Task<ActionResult<Tariff>> GetTariff(int tariffId)
+        {
+            var connectionString = _config.GetConnectionString("DbConnection");
+            await using var connection = new MySqlConnection(connectionString);
+            var tariff = await connection.QueryFirstOrDefaultAsync<Tariff>("select * from Tariff where TariffId = @Id",
+                new { Id = tariffId });
+            if (tariff is null)
+            {
+                return BadRequest($"Id {tariffId} does not exist.");
+            }
+
+            return Ok(tariff);
+        }
+    }
+}
--- Beltelecom/Program.cs
+++ Beltelecom/Program.cs
@@ -1,4 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSwaggerGen();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -8,6 +9,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
